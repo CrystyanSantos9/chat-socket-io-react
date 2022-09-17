a781a3bea465d57929d54f131f8ba55abc084b3b
Crystyan Santos <cryssantos9@gmail.com>
2022-09-17 00:41:08 +0000

    listando mensagens e mandando audio

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { useEffect, useCallback } from "react";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import "./chatsocket-io.webflow.css";
+import React from 'react'
+import { useEffect, useCallback } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import './chatsocket-io.webflow.css'
 
 const Room = (props) => {
-  let params = useParams();
-  const [roomId, setRoomId] = useState("");
-  const [errors, setErros] = useState("");
+  let params = useParams()
+  const [roomId, setRoomId] = useState('')
+  const [errors, setErros] = useState('')
+  const [audioPermission, setAudioPermission] = useState(false)
+
+  let mediaRecorder = {}
+  navigator.mediaDevices.getUserMedia({ audio: true }).then(
+    (stream) => {
+      setAudioPermission(true)
+      mediaRecorder = new MediaRecorder(stream)
+      let chunks = []
+      mediaRecorder.ondataavailable = (data) => {
+        // data received
+        chunks.push(data.data)
+      }
+      mediaRecorder.onstop = () => {
+        //data sent
+        //api file reader
+        const reader = new window.FileReader()
+        //crio um objeto binário, falando que dentro tem um aúdio
+        const blob = new Blob(chunks, { type: 'audio/ogg; codec=opus' })
+        //Para construir url com dados dentro usando base64
+        reader.readAsDataURL(blob)
+        //quando ele terminar de criar a url com dados dentro
+        reader.onloadend = () => {
+          props.socket.emit('sendAudio', {
+            data: reader.result,
+            room: roomId,
+          })
+        }
+        chunks = []
+      }
+    },
+    (err) => {
+      mediaRecorder = null
+      setAudioPermission(false)
+    },
+  )
 
   // const reload = () => {
   //   const path = location.pathname
@@ -16,81 +51,95 @@ const Room = (props) => {
   // }
 
   const errorHandling = useCallback(async () => {
-    await props.socket.once("error", (error) => {
-      if (error !== "") {
-        console.log(error);
-        setErros(error);
+    await props.socket.once('error', (error) => {
+      if (error !== '') {
+        console.log(error)
+        setErros(error)
       } else {
-        setErros("");
+        setErros('')
       }
-    });
-  }, [params]);
+    })
+  }, [params])
 
   useEffect(() => {
     if (params) {
       // ADICIONEI -- verifica ação no reload
       // trata o reload de página
       // roomId = '' vazio pega o id do histórico do router dom
-      if (roomId === "") {
-        const initialRoomId = params.room;
-        setRoomId(initialRoomId);
-        props.socket.emit("join", initialRoomId);
+      if (roomId === '') {
+        const initialRoomId = params.room
+        setRoomId(initialRoomId)
+        props.socket.emit('join', initialRoomId)
       }
 
       // trata a mudança de pagina
-      if (params.room !== roomId && roomId !== "") {
-        const newRoomId = params.room;
-        setRoomId(newRoomId);
-        props.socket.emit("join", newRoomId);
+      if (params.room !== roomId && roomId !== '') {
+        const newRoomId = params.room
+        setRoomId(newRoomId)
+        props.socket.emit('join', newRoomId)
       }
     }
-  }, [params, props.socket]);
+  }, [params])
+
+  function onMouseUp() {
+    console.log('On Mouse UP')
+    mediaRecorder.stop()
+  }
+
+  function onMouseDown() {
+    console.log('On Mouse DOWN')
+    mediaRecorder.start()
+  }
 
   function handleKey(event) {
-    if (roomId !== "" && event.keyCode === 13) {
-      props.socket.emit("sendMsg", {
+    if (roomId !== '' && event.keyCode === 13) {
+      props.socket.emit('sendMsg', {
         msg: event.target.value,
-        room: roomId,
-      });
+        room: roomId
+      })
       //limpa tela depois de enviar
-      event.target.value = "";
+      event.target.value = ''
     }
   }
 
-  function renderContent(msg){
-    // se o conteudo for texto 
+  function renderContent(msg) {
+    // se o conteudo for texto
     // recebe o objo msg { com prop type para derminar a renderizacao }
-    console.log(msg)
-    if(msg.type === 'text'){
+    if (msg.type === 'text') {
       return msg.message
-    }else{
-      return <audio audio={msg.message} controls="true"></audio>
+    } else {
+      return (
+        <audio controls>
+          <source src={msg.message} type="audio/ogg" />
+        </audio>
+      )
     }
   }
   // lidando com diferentes tipos de dado
-  function renderMessage(msg){
-      return(
-        <div key={msg._id}className="message">
-          <span className="author">{msg.author} - {msg.when}</span>
-          <br />
-          {/* // colocamos a exibição do conteudo aqui / para lidar com o formato de saída */}
-          <span className="msg-body">{renderContent(msg)}</span>
-        </div>    
-      )
+  function renderMessage(msg) {
+    return (
+      <div key={msg._id} className="message">
+        <span className="author">
+          {msg.author} - {msg.when}
+        </span>
+        <br />
+        {/* // colocamos a exibição do conteudo aqui / para lidar com o formato de saída */}
+        <span className="msg-body">{renderContent(msg)}</span>
+      </div>
+    )
   }
 
   return (
     <div className="room">
-      {errorHandling() && errors !== "" ? <h3>{errors}</h3> : ""}
+      {errorHandling() && errors !== '' ? <h3>{errors}</h3> : ''}
       <div className="messages">
-      <div className="message">
-        <span className="msg-body"> Estou na sala : {JSON.stringify(roomId)} </span>
-       {console.log(Object.values(props.msgs))}
-      </div>
-     
+        <div className="message">
+          <span className="msg-body"> Estou na sala : {JSON.stringify(roomId)} </span>
+          {props.msgs[roomId] && props.msgs[roomId].map(renderMessage)}
         </div>
+      </div>
       <div className="new-message-form w-form">
-        <form>
+        <form style={{ display: 'flex' }}>
           <textarea
             id="field"
             name="field"
@@ -100,28 +149,21 @@ const Room = (props) => {
             className="msg w-input"
             onKeyUp={handleKey}
           ></textarea>
-          <span style={{ display: "flex" }}>
-            <button type="button" className="send-audio w-button">
+          <span>
+            <button type="button" className="send-audio w-button" onMouseUp={onMouseUp} onMouseDown={onMouseDown}>
               Enviar
               <br />
               Áudio
             </button>
             {/* <!-- <button type="button" className="start-counter w-button">Contar tempo<br></button> --> */}
             <div className="slidecontainer">
-              <input
-                type="range"
-                min="1"
-                max="10"
-                defaultValue="1"
-                className="slider"
-                id="myRange"
-              />
+              <input type="range" min="1" max="10" defaultValue="1" className="slider" id="myRange" />
             </div>
           </span>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Room;
+export default Room
--- src/Rooms.jsx
+++ src/Rooms.jsx
@@ -1,127 +1,131 @@
-import React, { Component } from "react";
-import io from "socket.io-client";
+import React, { Component } from 'react'
+import io from 'socket.io-client'
 
-
-import {
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import Room from "./Room";
-import SelectRoom from "./SelectRoom";
+import { Routes, Route, Link } from 'react-router-dom'
+import Room from './Room'
+import SelectRoom from './SelectRoom'
 
 class Rooms extends Component {
   constructor(props) {
-    super(props);
-    const token = window.localStorage.getItem("token");
-    const socket = io("http://192.168.1.110:3001?token=" + token);
-   
+    super(props)
+    const token = window.localStorage.getItem('token')
+    const socket = io('http://192.168.1.110:3001?token=' + token)
 
     this.state = {
       rooms: [],
       msgs: [],
-    };
+    }
 
     //Eventos socket.io
 
-    socket.on("newMsg", (msg) => {
-      // se mensagem nao existe 
-      if(!this.state.msgs[msg.room]){
+    socket.on('newMsg', (msg) => {
+      // se mensagem nao existe
+      if (!this.state.msgs[msg.room]) {
         //pegamos tudo o que já existe no estado atual
         // manter o estado original imutável criando um novo objeto
-        const msgs = {...this.state.msgs}
+        const msgs = { ...this.state.msgs }
         //criando um posição dentro do novo estado para  mensagem nova e única
         // a posicao é o id da room, logo todas as mensagens ficarão associadas a room
         msgs[msg.room] = [msg]
         this.setState({
-          msgs: msgs
+          msgs: msgs,
         })
-      }else{
+      } else {
         //se a mensagem já existe
         // copiamos tudo que está no estado original
-        const msgs = {...this.state.msgs}
+        const msgs = { ...this.state.msgs }
         //e só adicionamos dentro no objeto de room
-        msgs[msg.room].push(msg) 
+        msgs[msg.room].push(msg)
         this.setState({
-          msgs: msgs
+          msgs: msgs,
         })
       }
-    });
+    })
 
     //recebe mensagem enviada e mostra na sala para todos
-    socket.on("newAudio", (msg) => {
-      // if (selectedRomm === msg.room) {
-      //   addMsg(msg)
-      // } else {
-      //   // atualizar contador de msgs não lidas
-      // }
-    });
-
-    //recebe mensagens quando clicar nas salas
-    socket.on("msgsList", (msgs) => {
-      
-      if(msgs.length > 0){
-        // copiamos tudo o que está no estado mensagens
-        const msgsTmp = {...this.state.msgs}
-        
-        // selecionamos a sala e carregamos as mensagens nela
-        msgsTmp[msgs[0].room] = msgs
-        const lista = msgsTmp[msgs[0].room].map(msg => msg.type)
-        console.log(lista)
+    socket.on('newAudio', (msg) => {
+      // se mensagem nao existe para essa lala que vai virm em msg 
+      if (!this.state.msgs[msg.room]) {
+        //pegamos tudo o que já existe no estado atual
+        // manter o estado original imutável criando um novo objeto
+        const msgs = { ...this.state.msgs }
+        //criando um posição dentro do novo estado para selecionar a sala que carregamos ao clicar em sala
+        // a posicao é o id da room, logo todas as mensagens ficarão associadas a room
+        msgs[msg.room] = [msg]
         this.setState({
-          //alterando o estado de msgs para as que estamos carregando do banco 
-          msgs: msgsTmp
+          msgs: msgs,
+        })
+      } else {
+        //se a mensagem já existe
+        // copiamos tudo que está no estado original
+        const msgs = { ...this.state.msgs }
+        //e só adicionamos dentro no objeto de room
+        msgs[msg.room].push(msg)
+        this.setState({
+          msgs: msgs,
         })
       }
-    });
+    })
+
+    //recebe mensagens quando clicar nas salas
+    socket.on('msgsList', (msgs) => {
+      if (msgs.length > 0) {
+        // copiamos tudo o que está no estado mensagens todas as mensagens
+        const msgsTmp = { ...this.state.msgs }
+        // selecionamos a sala e carregamos todas as mensagens dessa sala nele
+        msgsTmp[msgs[0].room] = msgs
+
+        this.setState({
+          //alterando o estado de msgs para as que estamos carregando do banco
+          msgs: msgsTmp,
+        })
+      }
+    })
 
     // evento para escutar a criação de nova sala
-    socket.on("newRoom", (room) => {
-      this.setState({ rooms: [...this.state.rooms], room });
-    });
+    socket.on('newRoom', (room) => {
+      this.setState({ rooms: [...this.state.rooms], room })
+    })
 
-    socket.on("roomList", (rooms) => {
-      this.setState({ rooms: rooms });
-    });
-
-    
+    socket.on('roomList', (rooms) => {
+      this.setState({ rooms: rooms })
+    })
 
     this.socket = socket
   }
-    
+
   render() {
     return (
       <div className="container w-container">
-       
         <div className="rooms">
           <h1 className="title-rooms">Salas Disponíveis</h1>
           <ul className="room-list w-list-unstyled">
-          {
-            this.state.rooms.map(room=>{
+            {this.state.rooms.map((room) => {
               return (
-              <li key={room._id} className="room-item">
-                <Link to={`/rooms/${room._id}`}>{room.name}</Link>
-              </li>
+                <li key={room._id} className="room-item">
+                  <Link to={`/rooms/${room._id}`}>{room.name}</Link>
+                </li>
               )
-            })
-          }
+            })}
             {/* <li className="room-item">Sala 2</li>
             <li className="room-item">
               Sala 3 <span className="notifications">(2)</span>
             </li> */}
           </ul>
-        
+
           <div className="add-room">+</div>
         </div>
-            
+
         <Routes>
-          <Route exact path="/" element={<SelectRoom/>} />
-          <Route path="/:room" element={<Room errors={this.state.errors} socket={this.socket} msgs={this.state.msgs} />} /> 
-        </Routes>    
-        
+          <Route exact path="/" element={<SelectRoom />} />
+          <Route
+            path="/:room"
+            element={<Room errors={this.state.errors} socket={this.socket} msgs={this.state.msgs} />}
+          />
+        </Routes>
       </div>
-    );
+    )
   }
 }
 
-export default Rooms;
+export default Rooms
